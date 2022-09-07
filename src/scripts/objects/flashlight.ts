c5aade6c2a7913b0466e52e88dec1566c0b76590
Guillaume Renaudin <dev@guillaumerenaudin.com>
2022-09-07 15:34:17 +0200

    :tada: first octopus !

--- src/scripts/objects/flashlight.ts
+++ src/scripts/objects/flashlight.ts
@@ -26,13 +26,9 @@ export default class Flashlight {
       0.5
     ).setDepth(3).setBlendMode(Phaser.BlendModes.ADD).setOrigin(x,y)
 
-    scene.physics.add.existing(this.polygon)
-    
   }
 
   public update() { 
-    this.polygon.setPosition(this.submarine.getBottomRight().x, this.submarine.getBottomRight().y - 40)
-    // this.polygon.body.position.x = this.submarine.getBottomRight().x
-     this.polygon.body.position.y = this.submarine.getBottomRight().y - 20 - Flashlight.LIGHT_SIZE * 20
+    this.polygon.setPosition(this.submarine.getBottomRight().x,this.submarine.getBottomRight().y)
   }
 }
